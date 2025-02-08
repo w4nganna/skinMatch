a0bbfed0b4a9a475c64c65de92a9d68831884c28
chenj926 <shendicjl@gmail.com>
2025-02-08 18:18:21 -0500

    add user entity

---
+++ src/main/java/com/example/cms/model/entity/User.java
@@ -0,0 +1,45 @@
+package com.example.cms.model.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "users")
+public class User {
+
+    @Id
+    @NotNull
+    // not sure if this is needed
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto-increment userId
+    private long userId;
+
+    @NotEmpty
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @NotEmpty
+    @Email // Ensures valid email format
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @NotEmpty
+    @Column(name = "password", nullable = false)
+    private String password;
+
+
+    public User(long userId, String name, String email, String password) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
