8bea5d6c743028f0a08133f80772203175d89007
chenj926 <shendicjl@gmail.com>
2025-02-19 02:51:48 -0500

    change userId type to string

--- src/main/java/com/example/cms/model/repository/UserRepository.java
+++ src/main/java/com/example/cms/model/repository/UserRepository.java
@@ -10,16 +10,16 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, String> {
 
     @Query(value = "SELECT u.userId, u.email FROM Users u " +
                     "WHERE u.userId = :userId", nativeQuery = true)
-    List<User> getUserIdAndEmail(@Param("userId") Long userId, @Param("email") String email);
+    List<User> getUserIdAndEmail(@Param("userId") String userId, @Param("email") String email);
 
     @Query(value =  "SELECT * FROM Users " +
                     "WHERE userId = :userId " +
                     "OR email = :email", nativeQuery = true)
-    List<User> checkUniqueUser(@Param("userId") Long userId, @Param("email") String email);
+    List<User> checkUniqueUser(@Param("userId") String userId, @Param("email") String email);
 
 
 }
