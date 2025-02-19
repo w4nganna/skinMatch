a72b8a00e54bb001f7fbb71309576b203328db9d
chenj926 <shendicjl@gmail.com>
2025-02-19 02:08:11 -0500

    update user controller

--- src/main/java/com/example/cms/controller/UserController.java
+++ src/main/java/com/example/cms/controller/UserController.java
@@ -1,11 +1,14 @@
-package com.example.cms.controller.exceptions;
+package com.example.cms.controller;
 
+import com.example.cms.controller.Dto.UserDto;
+import com.example.cms.controller.exceptions.UserNotFoundException;
 import com.example.cms.model.entity.User;
 import com.example.cms.model.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import javax.validation.constraints.Null;
+import java.util.*;
 
 @CrossOrigin
 @RestController
@@ -24,9 +27,81 @@ public class UserController {
     }
 
     @GetMapping("/users/{userId}")
-    public User getUserById(@PathVariable long userId) {
-        return userRepository.findByUserId(userId)
-                .orElseThrow(() -> new  );
+    public User getUserById(@PathVariable Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException(userId));
+    }
+
+    @GetMapping("/user/{userId}/{email}")
+    public Boolean getUserIdAndEmail(@PathVariable Long userId, @PathVariable String email) {
+        return userRepository.checkUniqueUser(userId, email) != null;
+    }
+
+    @PutMapping("/users/{password}")
+    public User updateUserPassword(@RequestBody UserDto userDto, @PathVariable("userId") Long userId) {
+        return userRepository.findById(userId)
+                .map(user -> {
+                    user.setPassword(userDto.getPassword());
+                    return userRepository.save(user);
+                })
+                .orElseGet(() -> {
+                    User newUser = new User();
+                    newUser.setUserId(userId);
+                    newUser.setName(userDto.getName());
+                    newUser.setPassword(userDto.getPassword());
+                    newUser.setEmail(userDto.getEmail());
+                    return userRepository.save(newUser);
+                });
+    }
+
+    @PutMapping("/users/{email}")
+    public User updateUserEmail(@RequestBody UserDto userDto, @PathVariable("userId") Long userId) {
+        return userRepository.findById(userId)
+                .map(user -> {
+                    user.setEmail(userDto.getEmail());
+                    return userRepository.save(user);
+                })
+                .orElseGet(() -> {
+                    User newUser = new User();
+                    newUser.setUserId(userId);
+                    newUser.setName(userDto.getName());
+                    newUser.setPassword(userDto.getPassword());
+                    newUser.setEmail(userDto.getEmail());
+                    return userRepository.save(newUser);
+                });
+    }
+
+    @PutMapping("/users/{name}")
+    public User updateUserName(@RequestBody UserDto userDto, @PathVariable("userId") Long userId) {
+        return userRepository.findById(userId)
+                .map(user -> {
+                    user.setName(userDto.getName());
+                    return userRepository.save(user);
+                })
+                .orElseGet(() -> {
+                    User newUser = new User();
+                    newUser.setUserId(userId);
+                    newUser.setName(userDto.getName());
+                    newUser.setPassword(userDto.getPassword());
+                    newUser.setEmail(userDto.getEmail());
+                    return userRepository.save(newUser);
+                });
+    }
+
+    @PostMapping("/users")
+    public User createUser(@RequestBody UserDto userDto) {
+        User newUser = new User();
+        newUser.setUserId(userDto.getUserId());
+        newUser.setName(userDto.getName());
+        newUser.setPassword(userDto.getPassword());
+        newUser.setEmail(userDto.getEmail());
+
+        return userRepository.save(newUser);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable Long userId) {
+        userRepository.deleteById(userId);
     }
 
 }
