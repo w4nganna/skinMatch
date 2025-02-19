4af47501c148260aa0e8bcb0d6bb61336796fae0
chenj926 <shendicjl@gmail.com>
2025-02-19 02:06:48 -0500

    add user dto

--- src/main/java/com/example/cms/controller/Dto/UserDto.java
+++ src/main/java/com/example/cms/controller/Dto/UserDto.java
@@ -1,4 +1,13 @@
 package com.example.cms.controller.Dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserDto {
+    private long userId;
+    private String name;
+    private String password;
+    private String email;
 }
