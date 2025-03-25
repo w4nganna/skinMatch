4d04e4a70418076770da17a6eab45e1670a00b4a
PragyaPrakash0511 <pragya.prakash@mail.utoronto.ca>
2025-03-25 16:06:17 -0400

    Update ReviewTests.java

--- src/test/java/com/example/cms/ReviewTests.java
+++ src/test/java/com/example/cms/ReviewTests.java
@@ -1,7 +1,12 @@
 package com.example.cms;
 
-/**import com.example.cms.model.entity.User;
+import com.example.cms.model.entity.Review;
+import com.example.cms.model.entity.ReviewKey;
+import com.example.cms.model.entity.User;
+import com.example.cms.model.entity.Product;
+import com.example.cms.model.repository.ReviewRepository;
 import com.example.cms.model.repository.UserRepository;
+import com.example.cms.model.repository.ProductRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.junit.jupiter.api.Test;
@@ -17,12 +22,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-class ProductTests {
+class ReviewTests {
 
     @Autowired
     private MockMvc mockMvc;
@@ -31,42 +35,53 @@ class ProductTests {
     private ObjectMapper objectMapper;
 
     @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @Test
-    void addUser() throws Exception {
-        ObjectNode userJson = objectMapper.createObjectNode();
-        userJson.put("userId", "00003");
-        userJson.put("email", "test.user@mail.com");
-        userJson.put("password", "test1234");
+    void addReview() throws Exception {
+        ReviewKey reviewKey = new ReviewKey("00001", 1L);
+        ReviewKey reviewKey2 = new ReviewKey("00002", 2L);
+
+        ObjectNode reviewJson = objectMapper.createObjectNode();
+        reviewJson.put("userId", "00001");
+        reviewJson.put("productId", 1L);
+        reviewJson.put("reviewBody", "This product was perfect for my skin");
+        reviewJson.put("score", 5);
+        reviewJson.put("date", "2025-Jan-05");
 
         MockHttpServletResponse response = mockMvc.perform(
-                        post("/users").
-                                contentType("application/json").
-                                content(userJson.toString()))
+                        post("/reviews")
+                                .contentType("application/json")
+                                .content(reviewJson.toString()))
                 .andReturn().getResponse();
 
         assertEquals(200, response.getStatus());
-        assertTrue(userRepository.findById("00003").isPresent());
-        User addedUser = userRepository.findById("00003").get();
-
-        assertEquals("00003", addedUser.getUserId());
-        assertEquals("test.user@mail.com", addedUser.getEmail());
+        assertTrue(reviewRepository.findById(reviewKey).isPresent());
     }
 
     @Test
-    void deleteUser() throws Exception {
-        User user = new User("00004", "delete.me@mail.com", "delete123");
-        userRepository.save(user);
+    void deleteReview() throws Exception {
+        ReviewKey reviewKey = new ReviewKey("00001", 1L);
+        Review review = new Review();
+        review.setReviewId(reviewKey);
+        review.setUser(userRepository.findById("00001").orElseThrow());
+        review.setProduct(productRepository.findById(1L).orElseThrow());
+        review.setReviewBody("This product was perfect for my skin");
+        review.setScore(5);
+        review.setDate("2025-Jan-05");
+        reviewRepository.save(review);
 
         MockHttpServletResponse response = mockMvc.perform(
-                        delete("/users/00004").
-                                contentType("application/json"))
+                        delete("/reviews/00001/1"))
                 .andReturn().getResponse();
 
         assertEquals(200, response.getStatus());
-        assertTrue(userRepository.findById("00004").isEmpty());
+        assertTrue(reviewRepository.findById(reviewKey).isEmpty());
     }
 }
-
-*/
